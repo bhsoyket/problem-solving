05eb7d007ce8dbc0e4a0a58bd980e73c21830e9b
Belayet <belayet.hossain@apsissolutions.com>
2022-01-25 21:20:55 +0600

    Combine Two Tables

---
+++ leetCode/combine -two-ables.js
@@ -0,0 +1,17 @@
+// Combine Two Tables 
+/*
+Problem: Write an SQL query to report the first name, last name, city, and state of each person in the Person table.
+         If the address of a personId is not present in the Address table, report null instead.
+
+*/
+
+// SQL
+SELECT 
+    Person.firstname,
+    Person.lastname,
+    Address.city,
+    Address.state
+FROM
+    Person
+LEFT JOIN Address ON 
+    Address.personId = Person.personId;
